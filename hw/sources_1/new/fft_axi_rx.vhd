38cf67bcc1221395b38d87d86005e4bf790f8cf5
Justin Sapun <f006jnn@dartmouth.edu>
2025-06-06 01:49:19 -0400

    last try

--- hw/sources_1/new/fft_axi_rx.vhd
+++ hw/sources_1/new/fft_axi_rx.vhd
@@ -22,6 +22,8 @@ architecture Behavioral of fft_axi_rx is
 
 signal peak_bin_int :  STD_LOGIC_VECTOR(8 DOWNTO 0) := (others => '0');
 signal peak_freq_mag_int :  STD_LOGIC_VECTOR(31 DOWNTO 0) := (others => '0');
+signal max_mag_var :  unsigned(47 downto 0) := (others => '0');
+signal peak_bin_var :  STD_LOGIC_VECTOR(8 DOWNTO 0) := (others => '0');
 
 begin
 
@@ -30,15 +32,14 @@ s_axis_data_tready <= '1';
 
 process(s_axis_clk)
     variable mag_temp     : unsigned(47 downto 0);
-    variable max_mag_var  : unsigned(47 downto 0);
-    variable peak_bin_var : std_logic_vector(8 downto 0);
+    
 begin
     if rising_edge(s_axis_clk) then
         if s_axis_resetn = '0' then
             peak_freq_mag_int <= (others => '0');
             peak_bin_int      <= (others => '0');
-            max_mag_var       := (others => '0');
-            peak_bin_var      := (others => '0');
+            max_mag_var       <= (others => '0');
+            peak_bin_var      <= (others => '0');
 
         elsif s_axis_data_tvalid = '1' then
             
@@ -47,15 +48,15 @@ begin
                 unsigned(signed(s_axis_data_tdata(47 downto 24)) * signed(s_axis_data_tdata(47 downto 24)));
 
             if mag_temp > max_mag_var then
-                max_mag_var  := mag_temp;
-                peak_bin_var := s_axis_data_tuser(8 downto 0);
+                max_mag_var  <= mag_temp;
+                peak_bin_var <= s_axis_data_tuser(8 downto 0);
             end if;
 
             if s_axis_data_tlast = '1' then
                 peak_freq_mag_int <= std_logic_vector(max_mag_var(31 downto 0));  -- top 32 bits
                 peak_bin_int      <= peak_bin_var;
-                max_mag_var       := (others => '0');
-                peak_bin_var      := (others => '0');
+                max_mag_var       <= (others => '0');
+                peak_bin_var      <= (others => '0');
             end if;
         end if;
     end if;
